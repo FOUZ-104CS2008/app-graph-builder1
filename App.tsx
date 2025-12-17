2e73c42913f58f4d8285509d9e6728be7266b430
B FOUZIYA <110445840+FOUZ-104CS2008@users.noreply.github.com>
2025-12-17 19:56:38 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import AppLayout from "./layout/AppLayout";
+import GraphCanvas from "./components/GraphCanvas";
+
+export default function App() {
+  return (
+    <AppLayout>
+      <GraphCanvas />
+    </AppLayout>
+  );
+}
